c2939d91d358fd113f67f063015265601e222502
Dan Walkes <danwalkes@boulderai.com>
2021-05-25 20:52:09 +0000

    Add baicamerasrc support [EDGE-117]

--- docker/build.sh
+++ docker/build.sh
@@ -5,5 +5,8 @@ dockerfile=docker/Dockerfile.dGPU
 uname -m | grep aarch64 > /dev/null
 if [ $? -eq 0 ]; then
     dockerfile=docker/Dockerfile.Jetson
+    set -e
+    echo "authenticating to gcr using json key at /data/external-bai-*"
+    cat /data/keys/external-bai-*.json | docker login -u _json_key --password-stdin https://gcr.io
 fi
 docker build -t bai-deepstream -f ${dockerfile} src
