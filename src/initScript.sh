f824a35e3e25c23ae7f39c1457a40f2929cf3e5c
Dan Walkes <danwalkes@trellis-logic.com>
2023-10-30 01:51:23 +0000

    Move input file to data dir
    To support easily accessing the input video file (and modifying)
    use a video file in the data dir which is by default exported
    from the container.

--- src/initScript.sh
+++ src/initScript.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 cd $(dirname $0)
 if [ ! -d /data/cfg/deepstream ]; then
-	echo "deepstream config directory doesn't exist, creating from defaults"
-	mkdir -p /data/cfg/deepstream
+    echo "deepstream config directory doesn't exist, creating from defaults"
+    mkdir -p /data/cfg/deepstream
     chmod -R 777 /data
 fi
 cp --no-clobber -r cfg-deepstream-default/* /data/cfg/deepstream/
 mkdir -p /data/cfg/model
 mkdir -p /input
+
 if [ ! -e /input/video.mp4 ]; then
-    echo "No default video found at /input/video.mp4, using sample"
-    ln -s /opt/nvidia/deepstream/deepstream/samples/streams/sample_1080p_h265.mp4 /input/video.mp4
+    if [ ! -e /data/input/video.mp4 ]; then
+       echo "No default video found at /input/video.mp4, copying sample as input"
+       mkdir -p /data/input/
+       cp /opt/nvidia/deepstream/deepstream/samples/streams/sample_1080p_h265.mp4 /data/input/video.mp4
+    fi
+    ln -s /data/input/video.mp4 /input/video.mp4
 fi
 if [ ! -d /output ]; then
     echo "No dedicated output mount found, using /data/output dir"
