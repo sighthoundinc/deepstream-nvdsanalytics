52f69d105ff75feb8bee3fa6ce94cb303b82c85e
Dan Walkes <danwalkes@trellis-logic.com>
2023-10-30 01:45:18 +0000

    Add default-data dir if it doesn't exist
    Avoids failure on first attempt to run the run script with
    no args.

--- docker/run.sh
+++ docker/run.sh
@@ -8,11 +8,11 @@ if [ $? -ne 0 ]; then
     echo "To override location for persistent data dir, pass a volume mount as"
     echo "argument to this script, for instance with "
     echo "  \"-v /path/to/datadir:/data\""
+    mkdir -p data-default
     datamount="-v $(realpath data-default):/data"
 fi
 echo "Running docker with arguments ${datamount} $@"
 docker run --net=host --runtime nvidia \
-    -v ${data_dir}:/data/ \
     ${datamount} \
     $@ \
    -it deepstream-nvdsanalytics-docker
